f03c0d49ab9facdf6c531e26f590961248692695
slucaxx <l.sa@ba.estudante.senai.br>
2026-03-11 14:21:15 -0300

    Update questão10.py

--- questão10.py
+++ questão10.py
@@ -1,23 +1,41 @@
 import os
 os.system("cls")
 
-# ENTRADA
-print("-----------POSTO----------")
+# Entrada
+print("""
+---------------------TABELA DE DESCONTO---------------------
+         | Até 25 litros,desconto de 2% por litro
+ÁLCOOL   | acima de 25 litros,desconto do 4% por litros
+_______________________________________________________________-
+         | até 25 litos,desconto de 3% por litos
+GASOLINA | acima de 25 litos,desconto de 5% por litro
+   """)
+litros = float(input("Digite a quantidade de litros dejesado: "))
+tipo = input("Digite o tipo de combustível (A-álcool, G-gasolina): ")
 
-litros = float(input("Quantidade de litros: "))
-tipo = input("Tipo (A-Álcool, G-Gasolina): ")
+preco_gasolina = 6.59
+preco_alcool = 3.79
 
-preco_a = 3.79
-preco_g = 6.59
+val_final = 0
 
 if tipo == 'A':
-    total_bruto = litros * preco_a
-    desc = 0.03 if litros <= 20 else 0.05
-    valor_final = total_bruto * (1 - desc)
-    
-elif tipo == 'G':
-    total_bruto = litros * preco_g
-    desc = 0.04 if litros <= 20 else 0.06
-    valor_final = total_bruto * (1 - desc)
+    if litros <= 24:
+        desconto = 0.02
+    elif litros > 25:
+        desconto = 0.04 
+    val_final = litros * (preco_alcool *  desconto)
 
-print(f"Valor a pagar: R$ {valor_final:.2f}")
+
+elif tipo == 'G':
+    if litros <= 24:
+        desconto = 0.03 
+    elif litros > 25:
+        desconto = 0.05
+    val_final = litros * (preco_gasolina * desconto)
+
+else:
+    print("Tipo de combustível inválido!")
+
+# Saída
+if val_final > 0:
+    print(f"Total a pagar: R$ {val_final:.2f}")
