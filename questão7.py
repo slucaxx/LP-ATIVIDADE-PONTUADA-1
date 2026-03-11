5f91af28db1b578c6655b3eff47ed45e32a95674
slucaxx <l.sa@ba.estudante.senai.br>
2026-03-11 13:41:00 -0300

    Update questão7.py

--- questão7.py
+++ questão7.py
@@ -7,7 +7,7 @@ print("------- ANALISE DE PRODUTOS -------")
 
 nome = input("Digite o nome do produto: ")
 quantidade = int(input("digite a quantidade desejada: "))
-preco = float(input("digite o Preço do profuto: "))
+preco = float(input("digite o Preço do produto: "))
 
 total = quantidade * preco
 
@@ -27,4 +27,5 @@ print(f"Você escolheu {quantidade} unidades.")
 print(f"Desconto : {percentual*100}%")
 print(f"Total a pagar: R$ {total_pa:.2f}")
     
+print("=========FIM===============")
 
