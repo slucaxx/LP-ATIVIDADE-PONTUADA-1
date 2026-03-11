d9b058473e53deae560c4708e866d417283dd7ee
slucaxx <l.sa@ba.estudante.senai.br>
2026-03-11 13:18:27 -0300

    Update questão9.py

--- questão9.py
+++ questão9.py
@@ -5,20 +5,21 @@ os.system("cls")
 print("-----------EMPRESTIMO----------")
 
 renda = float(input("Renda mensal: R$ "))
-valor_emprestimo = float(input("Valor total do empréstimo: R$ "))
+val_emp = float(input("Valor total do empréstimo: R$ "))
 parcelas = int(input("Número de prestações: "))
 
-valor_prestacao = valor_emprestimo / parcelas
+val_pre = val_emp/ parcelas
 
 
-condicao1 = valor_emprestimo <= (renda * 10)
+condicao1 = val_emp <= (renda * 10)
 condicao2 = valor_prestacao <= (renda * 0.30)
 
 if condicao1 and condicao2:
     print("Empréstimo CONCEDIDO!")
+     print("- Valor total excede 10x a renda.")
 else:
     print("Empréstimo NEGADO!")
-    if not condicao1:
-        print("- Valor total excede 10x a renda.")
-    if not condicao2:
-        print("- Valor da prestação excede 30% da renda.")
+    print("- Valor da prestação excede 30% da renda.")
+        
+
+print("=======FIM========")
